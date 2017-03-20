5e8de14808d212ff98604d5d125e9ca654e0749c
Gnex <gnex@vivaldi.net>
2017-03-20 23:44:19 +0100

    no message

---
+++ src/metoda/newtona/Main.java
@@ -0,0 +1,33 @@
+package metoda.newtona;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Punkt {
+    public double x, y;
+    public Punkt(double x, double y) {
+        this.x = x; this.y = y;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Punkt> punkty = new ArrayList<>();
+        double podanyX, podanyY;
+        int iloscPunktow;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ile punktów chcesz wprowadzić?");
+        iloscPunktow = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < iloscPunktow; i++) {
+            System.out.println("Podaj wartość X:");
+            podanyX = Double.parseDouble(sc.nextLine());
+            System.out.println("Podaj wartość Y:");
+            podanyY = Double.parseDouble(sc.nextLine());
+            punkty.add(new Punkt(podanyX,podanyY));
+            System.out.println("Punkt numer " + (i+1) + " został dodany!");
+        }
+    }
+}
